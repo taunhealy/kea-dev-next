862bec283344dd6f1b5916cd0009da40d3384628
taunhealy <taunhealy@gmail.com>
2025-04-02 11:54:20 +0200

    deskStructure

--- sanity/deskStructure.ts
+++ sanity/deskStructure.ts
@@ -1,12 +1,12 @@
-import type { StructureBuilder } from 'sanity/structure'
-import { orderableDocumentListDeskItem } from '@sanity/orderable-document-list'
-import { schemaTypes } from './schemaTypes'
+import type {StructureBuilder} from 'sanity/structure'
+import {orderableDocumentListDeskItem} from '@sanity/orderable-document-list'
+import {schemaTypes} from './schemaTypes'
 
 export const structure = (S: StructureBuilder, context: any) => {
   // Get all document types from schema
   const documentTypes = schemaTypes
-    .filter(schema => schema.type === 'document')
-    .map(schema => schema.name)
+    .filter((schema) => schema.type === 'document')
+    .map((schema) => schema.name)
 
   return S.list()
     .title('Content')
@@ -16,14 +16,15 @@ export const structure = (S: StructureBuilder, context: any) => {
         type: 'page',
         title: 'Pages',
         S,
-        context
+        context,
       }),
       // Automatically add all other document types
       ...documentTypes
-        .filter(typeName => typeName !== 'page') // Exclude page since we handled it above
-        .map(typeName => 
-          S.documentTypeListItem(typeName)
-            .title(typeName.charAt(0).toUpperCase() + typeName.slice(1))
-        )
+        .filter((typeName) => typeName !== 'page') // Exclude page since we handled it above
+        .map((typeName) =>
+          S.documentTypeListItem(typeName).title(
+            typeName.charAt(0).toUpperCase() + typeName.slice(1),
+          ),
+        ),
     ])
 }
