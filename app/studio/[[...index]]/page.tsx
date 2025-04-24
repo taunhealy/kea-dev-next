b209befd639712524b249f102573540b119694ac
taunhealy <taunhealy@gmail.com>
2025-04-24 12:46:09 +0200

    sanity studio link

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -3,6 +3,8 @@
 import { NextStudio } from "next-sanity/studio";
 // Import directly from the sanity folder to avoid version conflicts
 import config from "../../../sanity/sanity.config";
+// or using absolute path:
+// import config from "@/sanity/sanity.config";
 
 export default function StudioPage() {
   // Cast the config to any to bypass type checking
