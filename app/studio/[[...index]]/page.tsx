a6f48789046006859875101a1fb729843638f0c9
taunhealy <taunhealy@gmail.com>
2025-04-24 23:51:38 +0200

    sanity studio

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import { NextStudio } from "next-sanity/studio";
-// Use relative path instead of alias
-import config from "../../../sanity/sanity.config";
-// or using absolute path:
-// import config from "@/sanity/sanity.config";
+import config from "@/sanity/sanity.config";
 
 export default function StudioPage() {
-  // Cast the config to any to bypass type checking
   return <NextStudio config={config as any} />;
 }
